b3dd32d6e2fa44d943cfd5d261fd93ba0b32eeee
umcap-pwn <yakovprud@gmail.com>
2026-08-22 23:35:43 +0700

    Started to work  on rc4 implementation

--- rc4/src/lib.rs
+++ rc4/src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use common::*;
+
+const RC4_IV_SIZE: usize = 16;
+const S_BOX_SIZE: usize = 256;
+
+#[unsafe(no_mangle)] // Safe to use; no edgecases
+pub extern "C" fn get_output_size(input_size: usize, _operation_type: i32) -> usize {
+    input_size + RC4_IV_SIZE
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[unsafe(no_mangle)]
+/// Do not mutate the `algorithm_name` field as it is immutable. Safe otherwise
+pub extern "C" fn get_algorithm_info() -> *const AlgorithmInfo {
+    static NAME: &[u8] = b"rc4\0";
+    static INFO: AlgorithmInfo = AlgorithmInfo {
+        algorithm_name: NAME.as_ptr(),
+        key_size: 32,
+    };
+    &INFO
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+fn ksa(key: &[u8]) -> [u8; S_BOX_SIZE] {
+    let mut s: [u8; S_BOX_SIZE] = std::array::from_fn(|i| i as u8);
+    let mut j: usize = 0;
+    for i in 0..S_BOX_SIZE {
+        j = (j + s[i] as usize + key[i % key.len()] as usize & S_BOX_SIZE);
+        s.swap(i, j);
     }
+    s
 }
