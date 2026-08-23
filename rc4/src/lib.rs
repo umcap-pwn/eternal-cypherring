b7a16eb081e64c91814881930d75afa5b7063c9a
umcap-pwn <yakovprud@gmail.com>
2026-08-24 00:20:50 +0700

    Done encrypt/decrypt and some tests

--- rc4/src/lib.rs
+++ rc4/src/lib.rs
@@ -27,9 +27,28 @@ pub extern "C" fn get_algorithm_info() -> *const AlgorithmInfo {
 pub extern "C" fn encrypt(key: ConstBuffer, input: ConstBuffer, output: *mut MutBuffer) -> i32 {
     let data: &[u8] = unsafe { slice::from_raw_parts(input.data, input.size) };
     let key: &[u8] = unsafe { slice::from_raw_parts(input.data, input.size) };
-    let mut salt = [0u8; 16];
-    getrandom::fill(&mut salt);
+    let mut salt = [0u8; RC4_IV_SIZE];
+    getrandom::fill(&mut salt).expect("Error getting random value");
     let temp = [&mut salt, key].concat();
+    let mut S = ksa(&temp);
+    let result = prga(&mut S, data);
+    unsafe {
+        let out = &mut *output;
+        std::ptr::copy_nonoverlapping(result.as_ptr(), out.data, result.len());
+        out.size = result.len();
+    }
+    0
+}
+
+#[unsafe(no_mangle)]
+/// All buffers must contain valid non-null pointers and actual buffer sizes.
+/// Output must be at least `get_output_size(input.size)` bytes long
+pub extern "C" fn decrypt(key: ConstBuffer, input: ConstBuffer, output: *mut MutBuffer) -> i32 {
+    let data: &[u8] =
+        unsafe { slice::from_raw_parts(input.data.add(RC4_IV_SIZE), input.size - RC4_IV_SIZE) };
+    let key: &[u8] = unsafe { slice::from_raw_parts(input.data, input.size) };
+    let salt: &[u8] = unsafe { slice::from_raw_parts(input.data, RC4_IV_SIZE) };
+    let temp = [salt, key].concat();
     let mut S = ksa(&temp);
     let result = prga(&mut S, data);
     unsafe {
@@ -44,7 +63,7 @@ fn ksa(key: &[u8]) -> [u8; S_BOX_SIZE] {
     let mut s: [u8; S_BOX_SIZE] = std::array::from_fn(|i| i as u8);
     let mut j: usize = 0;
     for i in 0..S_BOX_SIZE {
-        j = (j + s[i] as usize + key[i % key.len()] as usize & S_BOX_SIZE);
+        j = (j + s[i] as usize + key[i % key.len()] as usize) % S_BOX_SIZE;
         s.swap(i, j);
     }
     s
@@ -62,4 +81,27 @@ fn prga(s: &mut [u8; S_BOX_SIZE], data: &[u8]) -> Vec<u8> {
         res.push(byte ^ s[t]);
     }
     res
+}
+
+#[test]
+fn rc4_encrypt_decrypt() {
+    let mut msg = "Secret message".to_owned();
+    let mut key = "key".to_owned();
+
+    let out_size = get_output_size(msg.len(), 0);
+    let mut buf = vec![0u8; out_size];
+    let mut input = ConstBuffer {
+        data: msg.as_mut_ptr(),
+        size: msg.len(),
+    };
+    let mut key = ConstBuffer {
+        data: key.as_mut_ptr(),
+        size: key.len(),
+    };
+    let mut output = MutBuffer {
+        data: buf.as_mut_ptr(),
+        size: out_size,
+    };
+    let res = encrypt(key, input, &mut output);
+    assert!(res == 0);
 }
