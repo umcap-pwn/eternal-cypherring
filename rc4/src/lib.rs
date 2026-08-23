72edc3b749a3716a9e31ef725905c5ba43a07196
umcap-pwn <yakovprud@gmail.com>
2026-08-23 19:07:39 +0700

    Done ksa, prga and encrypting

--- rc4/src/lib.rs
+++ rc4/src/lib.rs
@@ -1,3 +1,5 @@
+use std::{ptr::swap, slice};
+
 use common::*;
 
 const RC4_IV_SIZE: usize = 16;
@@ -19,6 +21,25 @@ pub extern "C" fn get_algorithm_info() -> *const AlgorithmInfo {
     &INFO
 }
 
+#[unsafe(no_mangle)]
+/// All buffers must contain valid non-null pointers and actual buffer sizes.
+/// Output must be at least `get_output_size(input.size)` bytes long
+pub extern "C" fn encrypt(key: ConstBuffer, input: ConstBuffer, output: *mut MutBuffer) -> i32 {
+    let data: &[u8] = unsafe { slice::from_raw_parts(input.data, input.size) };
+    let key: &[u8] = unsafe { slice::from_raw_parts(input.data, input.size) };
+    let mut salt = [0u8; 16];
+    getrandom::fill(&mut salt);
+    let temp = [&mut salt, key].concat();
+    let mut S = ksa(&temp);
+    let result = prga(&mut S, data);
+    unsafe {
+        let out = &mut *output;
+        std::ptr::copy_nonoverlapping(result.as_ptr(), out.data, result.len());
+        out.size = result.len();
+    }
+    0
+}
+
 fn ksa(key: &[u8]) -> [u8; S_BOX_SIZE] {
     let mut s: [u8; S_BOX_SIZE] = std::array::from_fn(|i| i as u8);
     let mut j: usize = 0;
@@ -27,4 +48,18 @@ fn ksa(key: &[u8]) -> [u8; S_BOX_SIZE] {
         s.swap(i, j);
     }
     s
+}
+
+fn prga(s: &mut [u8; S_BOX_SIZE], data: &[u8]) -> Vec<u8> {
+    let mut i: usize = 0;
+    let mut j: usize = 0;
+    let mut res = Vec::<u8>::new();
+    for byte in data {
+        i = (i + 1) % S_BOX_SIZE;
+        j = (j + s[i] as usize) % S_BOX_SIZE;
+        s.swap(i, j);
+        let t = (s[i] as usize + s[j] as usize) % 256;
+        res.push(byte ^ s[t]);
+    }
+    res
 }
